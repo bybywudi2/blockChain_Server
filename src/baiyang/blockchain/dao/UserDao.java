c242360bfda823fda443496091a57c349f104054
白杨 <baiyangwd@163.com>
2018-11-06 17:45:33 +0800

    增加ip动态维护的相关代码

---
+++ src/baiyang/blockchain/dao/UserDao.java
@@ -0,0 +1,41 @@
+package baiyang.blockchain.dao;
+
+import baiyang.blockchain.domain.Block;
+import baiyang.blockchain.domain.ProblemBlock;
+import baiyang.blockchain.domain.User;
+
+import java.util.List;
+
+public interface UserDao {
+
+	void add(User user);
+
+	User find(String username, String password);
+
+	void addNewBolck(Block block,int nowid);
+
+	int blockHash(int id);
+
+	public boolean isHashRight(int id,String userId);
+	//查找注册的用户是否在数据库中存在
+	boolean find(String username);
+
+	public boolean findResIndex(String index,String mid);
+
+	public void addNewResBolck(ProblemBlock block, int nowid);
+
+	public String getMid();
+
+	public String getUserIp();
+
+	public int getUsersTotalNumber();
+
+	public List<String> getUserIps();
+
+	public void addUserCoin(int i,String uip);
+
+	public void addC(int i,String uip);
+
+	public boolean startCal(String uip);
+	public ProblemBlock getIndexBlock(int hash);
+}
